376cbde640a430aa60a00001135094a3a38cc70b
SahilK-027 <sahilkandhare07@gmail.com>
2024-03-06 12:47:45 +0530

    🦚 Add peacock particle animation

--- peacock/main.js
+++ peacock/main.js
@@ -5,6 +5,12 @@ import particlesFragmentShader from "./shaders/particles/fragment.glsl";
 import "./style.css";
 
 /**
+ * Constants
+ */
+const pariclesCount = 256;
+const planeSize = 12;
+
+/**
  * Base
  */
 // Canvas
@@ -62,6 +68,8 @@ scene.add(camera);
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
 
 /**
  * Renderer
@@ -75,9 +83,90 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(sizes.pixelRatio);
 
 /**
+ * 2D Displacement Map
+ */
+const displacement = {};
+// 2D canvas
+displacement.canvas2D = document.createElement("canvas");
+displacement.canvas2D.width = pariclesCount;
+displacement.canvas2D.height = pariclesCount;
+displacement.canvas2D.style.position = "fixed";
+displacement.canvas2D.style.width = "180px";
+displacement.canvas2D.style.height = "180px";
+displacement.canvas2D.style.top = 0;
+displacement.canvas2D.style.left = 0;
+displacement.canvas2D.style.zIndex = 10;
+// document.body.append(displacement.canvas2D);
+
+// 2D Context
+displacement.context = displacement.canvas2D.getContext("2d");
+displacement.context.fillRect(
+  0,
+  0,
+  displacement.canvas2D.width,
+  displacement.canvas2D.height
+);
+
+// Glow effect
+displacement.glowImage = new Image();
+displacement.glowImage.src = "./assets/glow.png";
+
+// Interactive plane
+displacement.interactivePlane = new THREE.Mesh(
+  new THREE.PlaneGeometry(planeSize, planeSize),
+  new THREE.MeshBasicMaterial({ color: "black", side: THREE.DoubleSide })
+);
+scene.add(displacement.interactivePlane);
+displacement.interactivePlane.visible = false;
+
+// Raycaster
+displacement.raycaster = new THREE.Raycaster();
+
+// Coordinates
+displacement.screenCursor = new THREE.Vector2(9999, 9999);
+displacement.canvasCursor = new THREE.Vector2(9999, 9999);
+displacement.canvasCursorPrevious = new THREE.Vector2(9999, 9999);
+
+window.addEventListener("pointermove", (event) => {
+  displacement.screenCursor.x = (event.clientX / sizes.width) * 2 - 1;
+  displacement.screenCursor.y = -(event.clientY / sizes.height) * 2 + 1;
+});
+
+// Texture
+displacement.texture = new THREE.CanvasTexture(displacement.canvas2D);
+
+/**
  * Particles
  */
-const particlesGeometry = new THREE.PlaneGeometry(10, 10, 256, 256);
+const particlesGeometry = new THREE.PlaneGeometry(
+  planeSize,
+  planeSize,
+  pariclesCount,
+  pariclesCount
+);
+particlesGeometry.setIndex(null);
+particlesGeometry.deleteAttribute("normal");
+
+const intensitiesArray = new Float32Array(
+  particlesGeometry.attributes.position.count
+);
+const anglesArray = new Float32Array(
+  particlesGeometry.attributes.position.count
+);
+
+for (let i = 0; i < particlesGeometry.attributes.position.count; i++) {
+  intensitiesArray[i] = Math.random();
+  anglesArray[i] = Math.random() * Math.PI * 2;
+}
+
+particlesGeometry.setAttribute(
+  "aIntensity",
+  new THREE.BufferAttribute(intensitiesArray, 1)
+);
+particlesGeometry.setAttribute(
+  "aAngle",
+  new THREE.BufferAttribute(anglesArray, 1)
+);
 
 const particlesMaterial = new THREE.ShaderMaterial({
   vertexShader: particlesVertexShader,
@@ -92,6 +181,7 @@ const particlesMaterial = new THREE.ShaderMaterial({
     uPictureTexture: new THREE.Uniform(
       textureLoader.load("./assets/image.png")
     ),
+    uDisplacementTexture: new THREE.Uniform(displacement.texture),
   },
 });
 const particles = new THREE.Points(particlesGeometry, particlesMaterial);
@@ -103,6 +193,51 @@ scene.add(particles);
 const tick = () => {
   // Update controls
   controls.update();
+
+  /**
+   * Raycaster
+   */
+  displacement.raycaster.setFromCamera(displacement.screenCursor, camera);
+  const intersections = displacement.raycaster.intersectObject(
+    displacement.interactivePlane
+  );
+  if (intersections.length) {
+    const uv = intersections[0].uv;
+    displacement.canvasCursor.x = uv.x * displacement.canvas2D.width;
+    displacement.canvasCursor.y = (1 - uv.y) * displacement.canvas2D.height;
+  }
+
+  // Fade out
+  displacement.context.globalCompositeOperation = "source-over";
+  displacement.context.globalAlpha = 0.02;
+  displacement.context.fillRect(
+    0,
+    0,
+    displacement.canvas2D.width,
+    displacement.canvas2D.height
+  );
+
+  // Speed alpha
+  const cursorDistance = displacement.canvasCursorPrevious.distanceTo(
+    displacement.canvasCursor
+  );
+  displacement.canvasCursorPrevious.copy(displacement.canvasCursor);
+  const alpha = Math.min(cursorDistance * 0.1, 1)
+
+  // Draw glow
+  const glowSize = displacement.canvas2D.width * 0.15;
+  displacement.context.globalCompositeOperation = "lighten";
+  displacement.context.globalAlpha = alpha;
+  displacement.context.drawImage(
+    displacement.glowImage,
+    displacement.canvasCursor.x - glowSize * 0.5,
+    displacement.canvasCursor.y - glowSize * 0.5,
+    glowSize,
+    glowSize
+  );
+
+  // Texture
+  displacement.texture.needsUpdate = true;
 
   // Render
   renderer.render(scene, camera);
--- peacock/shaders/particles/vertex.glsl
+++ peacock/shaders/particles/vertex.glsl
@@ -1,11 +1,32 @@
 uniform vec2 uResolution;
 uniform sampler2D uPictureTexture;
+uniform sampler2D uDisplacementTexture;
+
+attribute float aIntensity;
+attribute float aAngle;
+
 varying vec3 vColor;
 
 void main()
 {
+    // Displacement
+    vec3 newPosition = position;
+    float displacementIntensity = texture(uDisplacementTexture, uv).r;
+    displacementIntensity = smoothstep(0.1, 1.0, displacementIntensity);
+    vec3 displacement = vec3(
+        cos(aAngle) * 0.2,
+        sin(aAngle) * 0.2,
+        1.0
+    );
+    displacement = normalize(displacement);
+    displacement *= displacementIntensity;
+    displacement *= 3.0;
+    displacement *= aIntensity;
+
+    newPosition += displacement;
+
     // Final position
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
@@ -14,7 +35,7 @@ void main()
     float pictureIntensity = texture(uPictureTexture, uv).r;
 
     // Point size
-    gl_PointSize = 0.3 * pictureIntensity * uResolution.y;
+    gl_PointSize = 0.15 * pictureIntensity * uResolution.y;
     gl_PointSize *= (1.0 / - viewPosition.z);
 
     // Varyings
