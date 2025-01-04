960ec3f7cac6000f29fae9f9dda4d1b12dca8243
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2025-01-04 12:19:45 +0530

    ✨ Add pixel grid

--- Pixelated Transition/src/shaders/fragment.glsl
+++ Pixelated Transition/src/shaders/fragment.glsl
@@ -1,10 +1,18 @@
 uniform sampler2D uTexture;
-
 varying vec2 vUv;
 
-void main() {
-    vec2 TextureUV = vec2(vUv.x, vUv.y);
-    vec4 texture = texture2D(uTexture, TextureUV);
+// Function to calculate square distance
+float squareDistance(vec2 uv) {
+    vec2 p = abs(uv); // Absolute position relative to the center
+    return max(p.x, p.y); // Distance to the square edge
+}
 
+void main() {
+    // Adjusting the texture coordinates for proper centering and scaling
+    vec2 uv = (vUv - 0.5);
+    vec4 texture = texture2D(uTexture, vUv);
     gl_FragColor = texture;
+
+    float square = squareDistance(uv);
+    gl_FragColor = vec4(vec3(square), 1.0);
 }
