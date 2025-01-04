8fa34bed0100fa7fe75e456bc41bd1866e42e33d
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2025-01-04 21:59:03 +0530

    Add noise

--- Transition/src/shaders/fragment.glsl
+++ Transition/src/shaders/fragment.glsl
@@ -70,6 +70,72 @@ vec2 scaleUvs(vec2 uv, vec2 aspectCorrection) {
     return (uv - 0.5) * aspectCorrection + 0.5;
 }
 
+float hash(vec2 p) {
+    return fract(sin(dot(p, vec2(127.1, 311.7))) * 43758.5453123);
+}
+
+float floatNoise(vec2 uv) {
+    vec2 i = floor(uv);
+    vec2 f = fract(uv);
+
+    // Four corners of the cell
+    float a = hash(i);
+    float b = hash(i + vec2(1.0, 0.0));
+    float c = hash(i + vec2(0.0, 1.0));
+    float d = hash(i + vec2(1.0, 1.0));
+
+    // Smooth interpolation
+    vec2 u = f * f * (3.0 - 2.0 * f);
+
+    return mix(a, b, u.x) +
+        (c - a) * u.y * (1.0 - u.x) +
+        (d - b) * u.x * u.y;
+}
+vec3 mod289(vec3 x) {
+    return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec2 mod289(vec2 x) {
+    return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec3 permute(vec3 x) {
+    return mod289(((x * 34.0) + 1.0) * x);
+}
+
+float simplexNoise(vec2 v) {
+    const vec4 C = vec4(0.211324865405187,  // (3.0-sqrt(3.0))/6.0
+    0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
+    -0.577350269189626,  // -1.0 + 2.0 * C.x
+    0.024390243902439); // 1.0 / 41.0
+    vec2 i = floor(v + dot(v, C.yy));
+    vec2 x0 = v - i + dot(i, C.xx);
+
+    vec2 i1;
+    i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+    vec4 x12 = x0.xyxy + C.xxzz;
+    x12.xy -= i1;
+
+    i = mod289(i); // Avoid truncation effects in permutation
+    vec3 p = permute(permute(i.y + vec3(0.0, i1.y, 1.0)) + i.x + vec3(0.0, i1.x, 1.0));
+
+    vec3 m = max(0.5 - vec3(dot(x0, x0), dot(x12.xy, x12.xy), dot(x12.zw, x12.zw)), 0.0);
+    m = m * m;
+    m = m * m;
+
+    vec3 x = 2.0 * fract(p * C.www) - 1.0;
+    vec3 h = abs(x) - 0.5;
+    vec3 ox = floor(x + 0.5);
+    vec3 a0 = x - ox;
+
+    m *= 1.79284291400159 - 0.85373472095314 * (a0 * a0 + h * h);
+
+    vec3 g;
+    g.x = a0.x * x0.x + h.x * x0.y;
+    g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+    return 130.0 * dot(m, g);
+}
+
 void main() {
     // Calculate aspect correction
     vec2 aspectCorrection = vec2(1.0, uResolution.y / uResolution.x);
@@ -89,7 +155,12 @@ void main() {
     vec2 hexUv = barrelDistortionUvs * gridSize;
     vec4 hexCoords = hexCoordinates(hexUv);
     float hexDist = hexagonDistance(hexCoords.xy);
-    float hexBorder = smoothstep(0.47, 0.53, hexDist);
+    float hexBorder = smoothstep(0.47, 0.50, hexDist);
+
+    float y = pow(1.0 - max(0.0, 0.5 - hexDist), 10.0) * 1.5;
+    float z = simplexNoise(hexCoords.zw * 0.5);
+
+    gl_FragColor = vec4(vec3(z), 1.0);
 
     // Square pixel grid
     // float squareBorder = squareGrid(barrelDistortionUvs, gridSize, lineWidth);
@@ -105,7 +176,6 @@ void main() {
     // gl_FragColor = vec4(vec3(squareBorder), 1.0);
     // gl_FragColor = vec4(vec3(circleBorder), 1.0);
 
-    gl_FragColor = vec4(vec3(hexBorder), 1.0);
     // Mix circle + octagon patterns
     // float weightCircle = 0.25;
     // float weightOctagon = 0.35;
